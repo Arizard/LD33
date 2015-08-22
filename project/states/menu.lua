7962ef4f56f9dd24102aedc4a848610581c2af62
rukai <rubickent@gmail.com>
2015-08-22 23:34:57 +1000

    Added final graphics to the main menu

--- project/states/menu.lua
+++ project/states/menu.lua
@@ -14,6 +14,20 @@ function state:init()
 	end
 
 	TDF.AddClassToGameState( self, newButton )
+    
+
+    local dir = "assets/images/menu/"
+    self.images = {} -- store all images for the menu in here
+
+    --load static images
+    self.images.background = love.graphics.newImage( dir .. "background.png" )
+    self.images.knight = love.graphics.newImage( dir .. "bigStaticKnight.png" )
+    self.images.title = love.graphics.newImage( dir .. "titleText.png" )
+
+    --load animations
+    self.images.ghostSpritesheet = love.graphics.newImage( dir .. "bigGhostSpritesheet.png" )
+    local g = anim8.newGrid(199, 248, self.images.ghostSpritesheet:getWidth(), self.images.ghostSpritesheet:getHeight())
+    self.ghostAnimation = anim8.newAnimation(g('1-10', '1-3', '1-8', 4), 0.06)
 end
 
 function state:enter()
@@ -26,13 +40,15 @@ end
 
 function state:update( dt )
 	TDF.UpdateGameStateEntities( self, dt )
+    self.ghostAnimation:update(dt)
 end
 
 function state:draw( )
-	--love.graphics.print("Menu State", 32, 32)
-	TDF.DrawGameStateEntities( self )
+    love.graphics.setColor(255, 255, 255);
+    love.graphics.draw( self.images.background, 0, 0)
+	love.graphics.draw( self.images.title, love.graphics.getWidth()/2 - self.images.title:getWidth()/2 , 64 )
+	love.graphics.draw( self.images.knight, 0, 155 + 5*math.sin( TDF.Ticker/2 ) )
+    self.ghostAnimation:draw(self.images.ghostSpritesheet, 1024 - 250, 240 + 20*math.sin( TDF.Ticker ) )
 
-	love.graphics.draw( TDF.Images.Title_Title, 512-(720/2), 64 )
-	love.graphics.draw( TDF.Images.Knight_Title, 0, 5 + 5*math.sin( TDF.Ticker/2 ) )
-	love.graphics.draw( TDF.Images.Ghost_Title, 1024 - 250, 128 + 20*math.sin( TDF.Ticker ) )
+	TDF.DrawGameStateEntities( self )
 end
