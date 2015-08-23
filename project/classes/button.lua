fd4745e39d71c09d38efdb9b916ed9396e5fc97a
rukai <rubickent@gmail.com>
2015-08-23 18:11:43 +1000

    Added graphic for the play button on the menu

--- project/classes/button.lua
+++ project/classes/button.lua
@@ -7,18 +7,16 @@ Button = TDF.Class{
 		self.down = false
 		self.text = "Button"
 		self.font = TDF.Fonts.MainMedium
+
+        self.images = {}
+        dir = "assets/images/menu/"
+        --set these to images to draw an image instead of primitives + text
+        self.images.hover = nil
+        self.images.default = nil
 	end
 }
 
 function Button:draw()
-	
-	--love.graphics.rectangle( "line", self.x, self.y, self.w, self.h )
-
-	--if self.down == true then
-		--love.graphics.rectangle( "fill", self.x, self.y, self.w, self.h )
-	--end
-
-	--print("meme")
 	love.graphics.setFont( self.font )
 
 	if self.state == 0 then
@@ -29,29 +27,35 @@ function Button:draw()
 	    self:DrawDown( self.x, self.y, self.w, self.h )
 	end
 
-	self:DrawOverlay( self.x, self.y, self.w, self.h )
 end
 
 function Button:DrawOpen(x,y,w,h)
-	love.graphics.setColor( 155,125,125, 255 )
-	love.graphics.rectangle( "line", x, y, w, h )
+    if self.images.default == nil then
+        love.graphics.setColor( 155,125,125, 255 )
+        love.graphics.rectangle( "line", x, y, w, h )
+        self:DrawText( self.x, self.y, self.w, self.h )
+    else
+        love.graphics.draw(self.images.default, x, y)
+    end
 end
 function Button:DrawHover(x,y,w,h)
-	love.graphics.setColor( 255,175,175, 255 )
-	love.graphics.rectangle( "line", x, y, w, h )
+    if self.images.default == nil then
+        love.graphics.setColor( 255,175,175, 255 )
+        love.graphics.rectangle( "line", x, y, w, h )
+    else
+        love.graphics.draw(self.images.hover, x, y)
+    end
 end
 function Button:DrawDown(x,y,w,h)
-	love.graphics.setColor( 255,175,175, 255 )
-	love.graphics.rectangle( "fill", x, y, w, h )
+    if self.images.default == nil then
+        love.graphics.setColor( 255,175,175, 255 )
+        love.graphics.rectangle( "fill", x, y, w, h )
+    else
+        love.graphics.draw(self.images.hover, x, y)
+    end
 end
-function Button:DrawDisabled(x,y,w,h)
-	self:DrawOpen()
-end
-function Button:DisabledOverlay(x,y,w,h)
-	love.graphics.setColor( 0,0,0,150 )
-	love.graphics.rectangle( "fill", x, y, w, h )
-end
-function Button:DrawOverlay(x,y,w,h)
+
+function Button:DrawText(x,y,w,h)
 	love.graphics.setColor( 255,255,255 )
 	love.graphics.print(self.text, x+w/2 - love.graphics.getFont():getWidth( self.text )/2, y + h/2 - love.graphics:getFont():getHeight( self.text )/2 )
 end
@@ -59,13 +63,11 @@ end
 function Button:SetFont( font )
 	self.font = font
 end
-
 function Button:SetText( str )
 	self.text = str
 end
 
 function Button:update()
-	--print("meme update")
 	local mx, my = love.mouse.getX(), love.mouse.getY()
 
 	if mx > self.x and mx < self.x + self.w and my > self.y and my <  self.y + self.h then
